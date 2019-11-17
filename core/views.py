5230dcc3dcc8e075a3e63212ff7554abd6b43fdf
WhiteOwl314 <parksj914@naver.com>
2019-11-17 13:19:22 +0900

    connect HomeView urls

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views import View
+from django.views.generic.base import TemplateView
 
 
-class HomeView(View):
-    return render(request, "index.html")
+class HomeView(TemplateView):
+    template_name = "index.html"
