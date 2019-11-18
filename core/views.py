f2aa8232a2ed4c9a7c659c7662e83862e41a3078
WhiteOwl314 <parksj914@naver.com>
2019-11-18 18:56:24 +0900

    plus views's context

--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from exams.models import Exam
 
 
 class HomeView(TemplateView):
     template_name = "HomeView/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["exams"] = Exam.objects.all()
+        return context
