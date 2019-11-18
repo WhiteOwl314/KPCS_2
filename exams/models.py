18c0164d8da7c8a7e92a4009ae53a86d110fefb8
WhiteOwl314 <parksj914@naver.com>
2019-11-18 18:28:31 +0900

    fix_datefield

--- exams/models.py
+++ exams/models.py
@@ -6,7 +6,8 @@ from django.utils import timezone
 class Exam(models.Model):
     name = models.CharField(max_length=50)
     examDate = models.DateField(auto_now_add=False)
-    receptionDate = models.DurationField()
+    receptionDate_created = models.DateField(auto_now_add=False)
+    receptionDate_ended = models.DateField(auto_now_add=False)
 
     def __str__(self):
         return self.name
